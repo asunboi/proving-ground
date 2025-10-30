e7b1c4e6ca21a68b190e261bdcbdba5f20220456
Alexander Sun <asun@scripps.edu>
2025-10-30 16:49:32 -0700

    added preprocessing script for STATE data

---
+++ 0_datasets/preprocess_state.py
@@ -0,0 +1,5 @@
+import scanpy as sc
+import matplotlib.pyplot as plt
+import pandas as pd
+
+boli_state = sc.read_h5ad("/gpfs/group/jin/skim/STATE/state/test_boli_NTsubset/processed_data/Boli_Perturb_CTX_edit_NTsubset_hvg.h5ad")
